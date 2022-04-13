c3e5ba9429ab3cdf6eec37e5a21cc2a5508a29da
unknown <williambrooks287@gmail.com>
2022-04-13 17:15:10 -0400

    conditionally render nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter  } from '../../utils/helpers';
 
-const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-];
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
 
-function Nav() {
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
     return (
         <header>
@@ -38,12 +31,18 @@ function Nav() {
                     </li>
                     {categories.map((category) => (
                         <li
-                            className='mx-1'
-                            key={category.name}
+                        className={`mx-1 ${
+                            currentCategory.name === category.name && 'navActive'
+                            }`}
+                        key={category.name}
                         >
-                            <span onClick={() => categorySelected(category.name)} >
-                                {category.name}
-                            </span>
+                        <span
+                            onClick={() => {
+                            setCurrentCategory(category)
+                            }}
+                        >
+                            {capitalizeFirstLetter(category.name)}
+                        </span>
                         </li>
                     ))}
                 </ul>
